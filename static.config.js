f7e5c3277a6d27389b359eefedbcee069355fcee
Adam Demirel <adxm@msn.com>
2019-11-23 11:25:40 +1100

    Add table test2

--- static.config.js
+++ static.config.js
@@ -54,18 +54,18 @@ export default {
         };
       }
     },
-    {
-      path: "happiness",
-      template: "src/pages/post.js",
-      getData: async () => {
-        const post = await getPost("happiness");
+    // {
+    //   path: "happiness",
+    //   template: "src/pages/post.js",
+    //   getData: async () => {
+    //     const post = await getPost("happiness");
 
-        return {
-          title: "Happiness",
-          content: post,
-          path: "happiness"
-        }
-      }
-    }
+    //     return {
+    //       title: "Happiness",
+    //       content: post,
+    //       path: "happiness"
+    //     }
+    //   }
+    // }
   ]
 };
